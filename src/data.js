c5ef41752823b5ee261c10ce0680cdd6e10836a7
ninobrown585 <adrian.brown1@g.austincc.edu>
2021-07-28 16:41:53 -0500

    it worksgit add .

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export default
+    {
+        
+        name :'Adrian Brown',
+        headerTagline: [
+                        'I am Adrian Brown.',
+                        'a full stack web developer',
+                        'from Austin'
+    ],
+        contactEmail:'ninobrown58518@gmail.com',
+        resume: "./resume.zip",
+        abouttext: "I am currently employed as a Webmaster with experience as a Full-Stack Developer.",
+        aboutImage:'images/faceSHot3.jpg',
+       ShowAboutImage:true,
+ 
+       projects:[
+        {
+            id: 1,
+            title: 'Developer Quiz',
+            service: 'An app that challenges developers',
+            imageSrc: "../images/Developer-Quiz.PNG",
+            url: 'https://ninobrown585.github.io/Developer-Quiz-Challenge/'
+        },
+        {
+            id: 2,
+            title: 'Theatre-Aid',
+            service: 'link movies with astrology',
+            imageSrc: "../images/theatre-ade.PNG",
+            url: 'https://emilymetzgar.github.io/theatre-ade/'
+        }
+                        
+                
+               
+        ],
+        social: [
+
+            {   name:'Github',
+                url:'https://github.com/ninobrown585?tab=repositories'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/adrian-brown-655b05b2/'
+            },
+            
+
+        ]
+    }
